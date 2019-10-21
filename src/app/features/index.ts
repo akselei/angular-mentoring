a2e320211a26c792de804fad0d89f27a7b58b5b6
Oleksii_Yenik <oleksii_yenik@epam.com>
2019-10-21 15:41:25 +0300

    Fixed project structure

--- src/app/features/index.ts
+++ src/app/features/index.ts
@@ -3,13 +3,15 @@ import { LogoutComponent } from './authorization/logout/logout.component';
 import { SearchComponent } from './courses/search/search.component';
 import { ButtonCoursesComponent } from '@features/courses/button-courses/button-courses.component';
 import { CourseItemComponent } from '@features/courses/course-item/course-item.component';
+import { CoursesComponent } from '@features/courses/courses.component';
 
 export const components: any[] = [
   LoginComponent,
   LogoutComponent,
   SearchComponent,
   ButtonCoursesComponent,
-  CourseItemComponent
+  CourseItemComponent,
+  CoursesComponent
 ];
 
 export * from './authorization/login/login.component';
@@ -17,3 +19,4 @@ export * from './authorization/logout/logout.component';
 export * from './courses/search/search.component';
 export * from './courses/button-courses/button-courses.component';
 export * from './courses/course-item/course-item.component';
+export * from './courses/courses.component'
--- src/app/shared/components/index.ts
+++ src/app/shared/components/index.ts
@@ -1,16 +1,13 @@
 import { HeaderComponent } from './header/header.component';
 import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
-import { CoursesComponent } from './courses/courses.component';
 import { FooterComponent } from './footer/footer.component';
 
 export const components: any[] = [
   HeaderComponent,
   BreadcrumbsComponent,
-  CoursesComponent,
   FooterComponent
 ];
 
 export * from './header/header.component';
 export * from './breadcrumbs/breadcrumbs.component';
-export * from './courses/courses.component';
 export * from './footer/footer.component';

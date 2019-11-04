2fc9b0117fa392f1ee577c1172c545d9e1f46f86
Oleksii_Yenik <oleksii_yenik@epam.com>
2019-11-04 14:16:46 +0200

    HomeTask3 added unit tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,15 +1,20 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { FooterComponent, HeaderComponent } from '@shared/components';
+import { FeaturesModule } from '@features/features.module';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        FeaturesModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        HeaderComponent,
+        FooterComponent
       ],
     }).compileComponents();
   }));
@@ -24,12 +29,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('angular-mentoring');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-mentoring app is running!');
   });
 });
--- src/app/features/courses/courses.component.spec.ts
+++ src/app/features/courses/courses.component.spec.ts
@@ -1,14 +1,31 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CoursesComponent } from './courses.component';
+import { BreadcrumbsComponent } from '@features/courses/breadcrumbs/breadcrumbs.component';
+import { SearchComponent } from '@features/courses/search/search.component';
+import { ButtonCoursesComponent } from '@features/courses/button-courses/button-courses.component';
+import { CourseItemComponent } from '@features/courses/course-item/course-item.component';
+import { FormsModule } from '@angular/forms';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
 
 describe('CoursesComponent', () => {
   let component: CoursesComponent;
   let fixture: ComponentFixture<CoursesComponent>;
+  let httpMock: HttpTestingController;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CoursesComponent ]
+        imports: [
+            FormsModule,
+            HttpClientTestingModule
+        ],
+          declarations: [
+              CoursesComponent,
+              BreadcrumbsComponent,
+              SearchComponent,
+              ButtonCoursesComponent,
+              CourseItemComponent
+          ]
     })
     .compileComponents();
   }));
@@ -17,9 +34,43 @@ describe('CoursesComponent', () => {
     fixture = TestBed.createComponent(CoursesComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    httpMock = TestBed.get(HttpTestingController);
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+      expect(component).toBeTruthy();
   });
+
+  it('should show console log for delete item', () => {
+      spyOn(component, 'deleteCourseById');
+
+      component.deleteCourseById(1);
+      expect(component.deleteCourseById).toHaveBeenCalledWith(1);
+  });
+
+  it('should return console log for Load More function', () => {
+      spyOn(component, 'loadMoreHandler');
+
+      const loadMore = fixture.debugElement.nativeElement.querySelector('.courses__content_load-more-link');
+      loadMore.click();
+
+      expect(component.loadMoreHandler).toHaveBeenCalled();
+  });
+
+  it('getJSON() should http GET courseList', () => {
+
+      const courseList = [
+          {
+              id: 1,
+              title: 'Title',
+              duration: '1h 28 min',
+              data: '9 Nov, 2018',
+              description: 'Hello'
+          }
+      ];
+
+      component.getJSON().subscribe((res) => {
+          expect(res).toEqual(courseList);
+      });
+    });
 });

53c0313ea9ec1605c91cd07014dc2af57b2a5ec5
Oleksii <aksselei@gmail.com>
2019-12-03 15:58:25 +0200

    HomeWork 7 finished

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,22 @@ import { Routes, RouterModule } from '@angular/router';
 import { CoursesComponent } from '@features/courses/courses.component';
 import { LoginComponent } from '@features/auth/login/login.component';
 import { AddNewCourseComponent } from '@features/add-new-course/components/add-new-course.component';
+import { AuthGuardService } from '@core/services/auth-guard/auth-guard.service';
 
 const routes: Routes = [
   { path: '', redirectTo: 'courses', pathMatch: 'full' },
   { path: 'courses', data: { breadcrumb: 'Courses' },
     children: [
-      { path: '', component: CoursesComponent },
-      { path: 'add-course', component: AddNewCourseComponent, data: { breadcrumb: 'New Course' } },
-      { path: ':id', component: AddNewCourseComponent, data: { breadcrumb: 'Edit Course' } }
+      { path: '',
+          component: CoursesComponent },
+      { path: 'add-course',
+          component: AddNewCourseComponent,
+          data: { breadcrumb: 'New Course' },
+          canActivate: [AuthGuardService] },
+      { path: ':id',
+          component: AddNewCourseComponent,
+          data: { breadcrumb: 'Edit Course' },
+          canActivate: [AuthGuardService] }
     ]
   },
   { path: 'login', component: LoginComponent }
--- src/app/core/services/auth-guard/auth-guard.service.ts
+++ src/app/core/services/auth-guard/auth-guard.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { AuthService } from '@core/services/auth/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuardService {
+export class AuthGuardService implements CanActivate {
 
-  constructor() { }
+  constructor(
+      private authService: AuthService,
+      private router: Router
+  ) { }
+
+  canActivate(): boolean {
+      if (!this.authService.isAuth()) {
+          alert('Should log in');
+
+          this.router.navigate(['/login']);
+          return false;
+      }
+
+      return true;
+  }
 }
